4ee841f6a0fa9b4b6f8701036cd70f9cffdbd2a1
Solyannik Olga <solyannikolga@gmail.com>
2020-11-04 19:08:22 +0200

    initial version

---
+++ encryption.rb
@@ -0,0 +1,22 @@
+require 'digest'
+
+array = ['MD5', 'SHA1']
+
+puts 'Введите слово или фразу для шифрования:'
+
+user_str = STDIN.gets.chomp
+
+puts 'Каким способом зашифровать:'
+
+array.each.with_index(1) do |name, index|
+  puts "#{index}. #{name}"
+end
+
+user_input = STDIN.gets.to_i
+puts 'Вот что получилось:'
+
+if user_input == 1
+  puts Digest::MD5.hexdigest(user_str)
+else
+  puts Digest::SHA1.hexdigest(user_str)
+end
